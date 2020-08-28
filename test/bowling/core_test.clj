afdc34fbc584e2aaf72ac930626b72ab64567a2a
Tom <thomas.spencer@codurance.com>
2020-08-28 23:49:35 +0100

    Add 9 0 scores

--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -6,6 +6,7 @@
   (testing "zero-game score"
     (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
   (testing "simple game score no spares or strikes"
-    (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
-
+    (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
+    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0])))
+    )
   )
