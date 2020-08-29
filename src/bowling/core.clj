d8335003527d8a4d21d95826d9e57ac8237d6aea
Tom <thomas.spencer@codurance.com>
2020-08-29 13:05:38 +0100

    Add test for test-frame-scores-test - 10:57 - clojure bowling

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,5 +1,12 @@
 (ns bowling.core
   (:gen-class))
 
+(defn to-frame-scores [frames]
+  )
+
+(defn group-frames [rolls]
+  (partition 2 rolls)
+  )
+
 (defn score [rolls]
-  (reduce + rolls))
+  (reduce + (to-frame-scores (group-frames rolls))))
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -2,11 +2,21 @@
   (:require [clojure.test :refer :all]
             [bowling.core :refer :all]))
 
+(deftest group-frames-test
+  (testing "can group rolls into frames"
+    (is (= [[1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]
+           (group-frames [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))))
+
+(deftest to-frame-scores-test
+  (testing ""))
+
 (deftest score-test
-  (testing "zero-game score"
+  '(testing "zero-game score"
     (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
-  (testing "simple game score no spares or strikes"
+  '(testing "simple game score no spares or strikes"
     (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
-    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0])))
+    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0]))))
+  '(testing "spare-game score"
+    (is (= 29 (score [9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
     )
   )
