00278605dd998ea1b8df51fe3eb50288c444585c
Tom <thomas.spencer@codurance.com>
2020-08-31 18:06:00 +0100

    Add passing map for frames to frame scores

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,12 +1,12 @@
 (ns bowling.core
   (:gen-class))
 
-(defn to-frame-scores [frames]
-  )
-
 (defn group-frames [rolls]
   (partition 2 rolls)
   )
+
+(defn to-frame-scores [frames]
+  (map #(reduce + %) frames))
 
 (defn score [rolls]
   (reduce + (to-frame-scores (group-frames rolls))))
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -8,7 +8,10 @@
            (group-frames [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))))
 
 (deftest to-frame-scores-test
-  (testing ""))
+  (testing "can map frames to frame scores"
+    (is (= [1 2 3 4 5 6 7 8 9 0]
+           (to-frame-scores [[ 1 0] [ 1 1] [ 1 2] [ 1 3] [ 1 4] [ 1 5] [ 1 6] [ 1 7] [ 1 8] [ 0 0]]))))
+  )
 
 (deftest score-test
   '(testing "zero-game score"
