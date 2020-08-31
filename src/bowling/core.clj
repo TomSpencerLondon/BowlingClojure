5297e8806b24e9bea155157306c64fdd17cb8642
Tom <thomas.spencer@codurance.com>
2020-08-31 18:43:58 +0100

    Add strike-extra-score method

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -9,15 +9,19 @@
   (and (= 10 (reduce + frame))
        (= 2 (count frame))))
 
-
 (defn spare-extra-score [remaining-frames]
   (first (first remaining-frames)))
 
+(defn strike-extra-score [remaining-frames]
+  (reduce + (take 2 (flatten remaining-frames))))
+
 (defn next-frame-score [[frame & other]]
   (let [simple-score (reduce + frame)]
-    (if (spare? frame)
-      (+ simple-score (spare-extra-score other))
-      simple-score)))
+    (if (= 10 (first frame))
+      (+ simple-score (strike-extra-score other))
+      (if (spare? frame)
+        (+ simple-score (spare-extra-score other))
+        simple-score))))
 
 (defn to-frame-scores [frames]
   (loop [remaining frames
@@ -26,9 +30,7 @@
     (if (empty? remaining)
       scores
       (recur (rest remaining)
-             (conj scores (next-frame-score remaining)))))
-  ;(map #(reduce + %) frames)
-  )
+             (conj scores (next-frame-score remaining))))))
 
 (defn score [rolls]
   (reduce + (to-frame-scores (group-frames rolls))))
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -18,11 +18,20 @@
           (is (= 4 (spare-extra-score [[4 3]]))))
           (is (= 9 (spare-extra-score [[9 0]]))))
 
+
+(deftest strike-extra-score-test
+  (testing "can calc the strike extra score given next frames"
+    (is (= 7 (strike-extra-score [[4 3]]))))
+  (is (= 2 (strike-extra-score [[1 1]]))))
+
+
 (deftest next-frame-score-test
   (testing "can calc next frame score for a simple frame"
     (is (= 5 (next-frame-score [[3 2]]))))
   (testing "can calc next frame score for a spare frame"
     (is (= 11 (next-frame-score [[5 5] [1 2]]))))
+  (testing "can calc next frame score for a strike frame"
+    (is (= 13 (next-frame-score [[10] [1 2]]))))
   )
 
 
@@ -32,7 +41,10 @@
            (to-frame-scores [[ 1 0] [ 1 1] [ 1 2] [ 1 3] [ 1 4] [ 1 5] [ 1 6] [ 1 7] [ 1 8] [ 0 0]]))))
   (testing "can map spare frame to frame score"
     (is (= [11 2 3 4 5 6 7 8 9 0]
-           (to-frame-scores [[1 9] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]])))))
+           (to-frame-scores [[1 9] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]]))))
+  '(testing "can map strike frame to frame score"
+    (is (= [12 2 3 4 5 6 7 8 9 0]
+           (to-frame-scores [[10] [1 1] [1 2] [1 3] [1 4] [1 5] [1 6] [1 7] [1 8] [0 0]])))))
 
 (deftest score-test
   (testing "zero-game score"
@@ -41,4 +53,7 @@
     (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
     (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0]))))
   (testing "spare-game score"
-    (is (= 29 (score [9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))))
+    (is (= 29 (score [9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
+  '(testing "strike-game score"
+    (is (= 30 (score [10 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]))))
+  )
